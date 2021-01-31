8444f604b6636472985262b21b9100e3cd8b5a92
Thomas Sjøgren <thomas@sjogrens.dk>
2021-02-01 00:14:01 +0100

    Create base for todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form @submit.prevent="save">
+      <label for="title">Title</label>
+      <input id="title" v-model="title" type="text" />
+      <label for="description">Description</label>
+      <textarea id="description" v-model="description"></textarea>
+      <button type="submit">Save</button>
+    </form>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo.title }}<br />{{ todo.description }}</li>
+    </ul>
+
+    <button @click="resetStorage">Reset Storage</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { mapState, mapActions } from 'vuex';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+
+  data() {
+    return {
+      title: '',
+      description: '',
+    };
+  },
+
+  computed: {
+    ...mapState(['todos']),
+  },
+
+  beforeMount() {
+    this.fetchTodos();
+  },
+
+  methods: {
+    ...mapActions(['fetchTodos', 'addTodo', 'resetStorage']),
+
+    save() {
+      this.addTodo({
+        title: this.title,
+        description: this.description,
+      });
+
+      this.title = '';
+      this.description = '';
+    },
+  },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,51 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const ls = window.localStorage;
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    todos: [],
+  },
+
+  mutations: {
+    SET_TODOS: (state, payload) => (state.todos = payload),
+
+    ADD_TODO: (state, payload) => state.todos.push(payload),
+
+    REMOVE_TODO: (state, payload) => state.todos.splice(state.todos.indexOf(payload), 1),
+
+    RESET_STORAGE: state => (state.todos = []),
+  },
+
+  actions: {
+    fetchTodos: async ({ commit }) => {
+      const todos = await ls.getItem('todos');
+      commit('SET_TODOS', todos ? JSON.parse(todos) : []);
+    },
+
+    addTodo: async ({ commit, dispatch }, todo) => {
+      await commit('ADD_TODO', todo);
+      dispatch('saveTodos');
+    },
+
+    removeTodo: async ({ commit, dispatch }, todo) => {
+      await commit('REMOVE_TODO', todo);
+      dispatch('saveTodos');
+    },
+
+    saveTodos: async ({ state }) => {
+      console.log('Saving todos');
+      await ls.setItem('todos', JSON.stringify(state.todos));
+    },
+
+    resetStorage: async ({ commit }) => {
+      await ls.removeItem('todos');
+      commit('RESET_STORAGE');
+    },
+  },
+
+  getters: {},
 });
