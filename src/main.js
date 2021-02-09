a0c05308d220e9e5c5db18b50adc2a540fea1e39
Thomas Sjøgren <thomas@sjogrens.dk>
2021-02-10 00:57:01 +0100

    Add Fontawesome and Bulma

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bulma/css/bulma.min.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
