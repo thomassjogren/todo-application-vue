3aec09f81db70d00ed4e1cee0fd228f42803578a
Thomas Sjøgren <thomas@sjogrens.dk>
2021-02-15 01:17:02 +0100

    Add slide down transition component

---
+++ src/components/SlideDownTranstion.vue
@@ -0,0 +1,30 @@
+<template>
+  <Transition name="slide-down" @before-enter="beforeEnter" @enter="enter" @leave="leave">
+    <slot />
+  </Transition>
+</template>
+
+<script>
+export default {
+  methods: {
+    beforeEnter(el) {
+      el.style.height = '0';
+    },
+
+    enter(el) {
+      el.style.height = `${el.scrollHeight}px`;
+    },
+
+    leave(el) {
+      el.style.height = '0';
+    },
+  },
+};
+</script>
+
+<style>
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: all 0.5s;
+}
+</style>
